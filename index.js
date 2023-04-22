cb0559127902b882debec75d5d26b9801d0ded07
Rick Zamora <richardzamora1952@gmail.com>
2023-04-22 09:48:05 -0500

    cambio3

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function laCajaDePandora(numero) {
+    if (numero % 2 == 0) {
+      // Convertir a binario si es par
+      return numero.toString(2);
+    } else {
+      // Convertir a hexadecimal si es impar
+      return numero.toString(16);
     }
-
-    function creadaxHefesto(){
-
-    }
+  }
+  
