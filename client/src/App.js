0c86211fd52ab763ad7480599264f125d8293658
joacobolzon <joaco.bolzon3@gmail.com>
2023-04-26 18:46:13 -0300

    app prendiendo, prueba tailwind

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import './App.css'
+
+/* <Routes>
+  <Route exact path="/" element={"La landing"} />
+  <Route path="/home" element={"El home"} />
+</Routes> */
+function App() {
+  return (
+    /* probando tailwind */
+    <div className="text-3xl font-bold underline">
+      HOLA MUNDO
+      </div>
+  );
+}
+
+export default App;
