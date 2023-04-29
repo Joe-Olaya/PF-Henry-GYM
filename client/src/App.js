c584af651dde9f8e090187a46cde03d00687e097
Rick Zamora <richardzamora1952@gmail.com>
2023-04-29 10:35:19 -0500

    register format
    fadelanto de formato de registro

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { Services, Header } from "./container";
 import { Navbar, Contact } from "./components";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Registration from "./components/Registration/Registration";
+
 
 const App = () => (
   <Routes>
@@ -18,6 +20,7 @@ const App = () => (
       }
     />
     <Route path="/contact" element={<Contact/>} />
+    <Route path="/Register" element={<div className=" flex w-full h-screen"><div className=" w-full flex items-center lg:w-1/2" ><Registration/></div><div className="hidden bg-gray-200 lg:flex w-1/2 h-full items-center justify-center"><div className="w-60 h-60 bg-gradient-to-t from-black to bg-yellow-500 rounded-full"/></div></div>} />
   </Routes>
 );
 
