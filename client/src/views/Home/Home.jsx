dbfcc7e83851e3a135c430bcb17385a538f0271c
joacobolzon <joaco.bolzon3@gmail.com>
2023-04-30 14:59:24 -0300

    Home

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,16 +1,83 @@
+import "./Home.css";
+
 const Home = () => {
   return (
-    <>
-      <div>
-        <section>
+    <div className="div_container">
+      <div className="div_container_exercises">
+        <section className="section_title">
           <h1>Supplies & Training</h1>
-          <h3>Welcome!</h3>
+          <h3>Welcome to our website!</h3>
         </section>
-        <section>
+        <section className="section_cards">
+
+            <h1>EXERCISES</h1>
+
+          <div className="card_container">
             
+            <div className="card_section">
+              <img src="" alt="SIN IMAGEN" />
+              <h2>Titulo del ejercicio</h2>
+            </div>
+            
+            <div className="card_section">
+              <img src="" alt="SIN IMAGEN" />
+              <h2>Titulo del ejercicio</h2>
+            </div>
+            
+            <div className="card_section">
+              <img src="" alt="SIN IMAGEN" />
+              <h2>Titulo del ejercicio</h2>
+            </div>
+            
+            <div className="card_section">
+              <img src="" alt="SIN IMAGEN" />
+              <h2>Titulo del ejercicio</h2>
+            </div>
+          </div>
+          
+          <div className="div_button">
+              <button className="button_Viewall">View more</button>
+          </div>
+        
         </section>
       </div>
-    </>
+      
+      <div className="div_container_store">
+        <section className="section_cards">
+          
+          <h1>STORE</h1>
+
+          <div className="card_container">
+            
+            <div className="card_section">
+              <img src="" alt="SIN IMAGEN" />
+              <h2>Titulo del item de la store</h2>
+            </div>
+            
+            <div className="card_section">
+              <img src="" alt="SIN IMAGEN" />
+              <h2>Titulo del item de la store</h2>
+            </div>
+            
+            <div className="card_section">
+              <img src="" alt="SIN IMAGEN" />
+              <h2>Titulo del item de la store</h2>
+            </div>
+            
+            <div className="card_section">
+              <img src="" alt="SIN IMAGEN" />
+              <h2>Titulo del item de la store</h2>
+            </div>
+            
+          </div>
+          
+            <div className="div_button">
+                <button className="button_Viewall">View more</button>
+            </div>
+        
+        </section>
+      </div>
+    </div>
   );
 };
 
