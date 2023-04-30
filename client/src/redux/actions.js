678e942a802a00406422f487c2e0b2db92e0350b
joacobolzon <joaco.bolzon3@gmail.com>
2023-04-30 16:36:04 -0300

    inicio redux

---
+++ client/src/redux/actions.js
@@ -0,0 +1,40 @@
+import {
+  GET_EXERCISES,
+  GET_EXERCISE_BY_ID,
+  GET_PRODUCTS,
+  GET_USERS,
+} from "./action_types";
+import axios from "axios";
+
+const URL = "http://localhost:3001";
+
+export const getExercises = () => {
+  return async function (dispatch) {
+    const exercisesData = (await axios.get(`${URL}/exercises`)).data;
+    dispatch({
+      type: GET_EXERCISES,
+      payload: exercisesData,
+    });
+  };
+};
+
+export const getExerciseById = (idExercise) => {
+  return async function (dispatch) {
+    const exerciseData = (await axios.get(`${URL}/exercises/${idExercise}`))
+      .data;
+    dispatch({
+      type: GET_EXERCISE_BY_ID,
+      payload: exerciseData,
+    });
+  };
+};
+
+export const getUsers = () => {
+  return async function (dispatch) {
+    const usersData = (await axios.get(`${URL}/users`)).data;
+    dispatch({
+      type: GET_USERS,
+      payload: usersData,
+    });
+  };
+};
