98063ec41e80276eb12417b0d600b82a8d56ed09
Violettta05 <davidrey20@outlook.es>
2023-05-01 16:44:24 -0500

    starting filters

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react'; 
+import {useSelector, useDispatch} from 'react-redux';
+import css from '../Filters/Filters.module.css'
+
+
 
 const Filters = () => {
+
+  const [muscle, setMuscle]=useState([]) 
+
+  const dispatch = useDispatch();
+  const exercise = useSelector(state=>state.exercises)
+
+ useEffect(()=> {
+},[])
+
+
   return (
-    <div>filters</div>
+  <div>
+    <div className={css.options} >
+      <button onClick={()=>console.log(exercise)}>ver estado</button>
+        <select>
+          <option hiden defaultValue> select muscle </option>
+         {muscle.map(i =>
+          <option value={i}> {i} </option>)}
+        </select >
+        <button onClick={()=>console.log(muscle)}></button>
+          <select>
+          </select>
+          <select>
+          </select>
+      </div>
+    </div>
   )
 }
 
