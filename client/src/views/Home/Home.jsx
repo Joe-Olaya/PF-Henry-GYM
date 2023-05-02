7851c7d9364a248df92d772a6ecd68d8e7ddac21
joacobolzon <joaco.bolzon3@gmail.com>
2023-05-02 20:57:37 -0300

    Home-modified

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,55 +1,60 @@
 import "./Home.css";
-import Pagination from "../../components/Pagination/Pagination";
-import Filters from "../../components/Filters/Filters.jsx";
-
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
-
+  const exercises = useSelector((state) => state.exercises);
+  const limitedExercises = exercises.slice(0, 4);
   return (
-    
     <div className="div_container">
-      <Filters
-      />
       <div className="div_container_exercises">
         <section className="section_title">
           <h1>Supplies & Training</h1>
-          <h3>Welcome to our website!</h3>
+          <h3>Wanna train something today?</h3>
+          <h3>See all the exercises we have for you</h3>
         </section>
         <section className="section_cards">
-
-            <h1>EXERCISES</h1>
-
+          <h1>EXERCISES</h1>
           <div className="card_container">
-            
-            <div className="card_section">
-              <img src="" alt="SIN IMAGEN" />
-              <h2>Titulo del ejercicio</h2>
-            </div>
-            
-            <div className="card_section">
-              <img src="" alt="SIN IMAGEN" />
-              <h2>Titulo del ejercicio</h2>
-            </div>
-            
-            <div className="card_section">
-              <img src="" alt="SIN IMAGEN" />
-              <h2>Titulo del ejercicio</h2>
-            </div>
-            
-            <div className="card_section">
-              <img src="" alt="SIN IMAGEN" />
-              <h2>Titulo del ejercicio</h2>
-            </div>
+            {limitedExercises.map((exercise) => {
+              return (
+                <div className="card_section" key={exercise.id}>
+                  <h2>{exercise.name}</h2>
+                  <h3>{exercise.muscle}</h3>
+                  <h3>{exercise.body_part}</h3>
+                  <img src={exercise.gif_url} alt="exercise_name" />
+                </div>
+              );
+            })}
           </div>
-          
+          {/* <div className="card_container">
+            <div className="card_section">
+              <img src="" alt="SIN IMAGEN" />
+              <h2>Titulo del ejercicio</h2>
+            </div>
+
+            <div className="card_section">
+              <img src="" alt="SIN IMAGEN" />
+              <h2>Titulo del ejercicio</h2>
+            </div>
+
+            <div className="card_section">
+              <img src="" alt="SIN IMAGEN" />
+              <h2>Titulo del ejercicio</h2>
+            </div>
+
+            <div className="card_section">
+              <img src="" alt="SIN IMAGEN" />
+              <h2>Titulo del ejercicio</h2>
+            </div>
+          </div> */}
+
           <div className="div_button">
-              <button className="button_Viewall">View more</button>
+            <button className="button_Viewall"><Link to='/exercises'>View more</Link></button>
           </div>
-        
         </section>
       </div>
-      
-      <div className="div_container_store">
+      {/*       <div className="div_container_store">
         <section className="section_cards">
           
           <h1>STORE</h1>
@@ -84,9 +89,8 @@ export const Home = () => {
         
         </section>
         //solo de prueba 
-        <div className="flex justify-center "><Pagination /></div> //
-      </div>
+      </div> */}
+      <div className="flex justify-center "></div>
     </div>
   );
 };
-
