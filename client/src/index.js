9b3c46b0345a6dbca7f93754c65c1ce9733ce85f
Joe-Olaya <joel.olaya@hotmail.com>
2023-05-02 19:04:14 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
+import ReactDOM from "react-dom"
 
 ReactDOM.render(
   <Provider store={store}>
