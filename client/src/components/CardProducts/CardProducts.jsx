f24f2dd3ca7b503d2216827e1706763e3184f548
kevindel92 <kevindel_15@hotmail.com>
2023-05-06 19:05:26 -0300

    cambio /store

--- client/src/components/CardProducts/CardProducts.jsx
+++ client/src/components/CardProducts/CardProducts.jsx
@@ -1,20 +1,18 @@
 import "./CardProducts.css";
 
 const CardProducts = (props) => {
+
     return(
         <div className="cardPrd">
-            <popup trigger={false}>
                 <img className="productImg" src={props.image}/>
-                </popup>
             <div className="infoPrd">
             <h1 className="titlePrd">
-            <p>Name: {props.name}</p>
+            <p>{props.name}</p>
             </h1>
             <div className="descriptionPrd">
                 <h1>Description: {props.description}</h1>
                 <h1>Price: {props.price}</h1>
             </div>
-
             </div>
         </div>
     )
