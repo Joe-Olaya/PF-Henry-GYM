01cb797f414b0c488dea941b255dcd220d5fc0bb
Joe-Olaya <80336993+Joe-Olaya@users.noreply.github.com>
2023-05-06 01:52:09 -0300

    Update headersaleHandlers.js

--- api/src/handlers/headersaleHandlers.js
+++ api/src/handlers/headersaleHandlers.js
@@ -1,36 +1,49 @@
-const {createHeadersale} = require('../controllers/headersaleControllers')
-const {createNewBody} = require('../controllers/bodysaleControllers')
-const {getProductById} = require('../controllers/productsControllers')
-const {getUserById} = require('../controllers/usersControllers')
+const { createHeadersale } = require("../controllers/headersaleControllers");
+const { createNewBody } = require("../controllers/bodysaleControllers");
+const { getProductById } = require("../controllers/productsControllers");
+const { getUserById } = require("../controllers/usersControllers");
 
-const createSaleHandler = async (req,res) => {
-    const {client_id, product_list} = req.body;
+const createSaleHandler = async (req, res) => {
+  const { client_id, product_list } = req.body;
+  try {
+    const client = await getUserById(client_id);
+    const header = await createHeadersale(
+      client.id,
+      client.name,
+      client.address
+    );
+    const bodies = await createBodySales(product_list, header.id);
+    res.status(200).json({
+      header,
+      bodies,
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const createBodySales = async (arr, headerId) => {
+  return new Promise(async (resolve, reject) => {
     try {
-        const client = await getUserById(client_id);
-        const header = await createHeadersale(client.id, client.name, client.address);
-        const bodies = await new Promise(async (resolve, reject) => {
-            try {
-              const resultados = [];
-              for (const elemento of product_list) {
-                let product = await getProductById(elemento.product_id)
-                let price = product.price * elemento.units
-                const resultado = await createNewBody(price.toFixed(2), elemento.units, product.id, header.id)
-                resultados.push(resultado);
-              }
-              resolve(resultados);
-            } catch (error) {
-              reject(error);
-            }
-          });
-        res.status(200).json({
-            header : header,
-            bodies: bodies
-        })
+      const resultados = [];
+      for (const elemento of arr) {
+        let product = await getProductById(elemento.product_id);
+        let price = product.price * elemento.units;
+        const resultado = await createNewBody(
+          price.toFixed(2),
+          elemento.units,
+          product.id,
+          headerId
+        );
+        resultados.push(resultado);
+      }
+      resolve(resultados);
     } catch (error) {
-        res.status(400).json(error)
+      reject(error);
     }
-}
+  });
+};
 
 module.exports = {
-    createSaleHandler
-}
+  createSaleHandler,
+};
