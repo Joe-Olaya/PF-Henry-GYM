1c79dc58f7c0adff90697e545c9cf16bd74c8d02
Rick Zamora <richardzamora1952@gmail.com>
2023-05-09 16:54:58 -0500

    stilo
    hshshj

--- client/src/components/FormProducts/FormProducts.jsx
+++ client/src/components/FormProducts/FormProducts.jsx
@@ -42,30 +42,43 @@ const FormProducts = () => {
     //aunque tambien se podria implementar
   };
   return (
-
-    <div> 
-      <form action="" style={{margin:'0 auto'}}>
+<div className=" flex w-full h-screen ">
+   <div className=" w-full flex items-center justify-center ">
+    <div className="max-w-[800px]  px-10 py-20 ">
+      <h1 className="text-5xl font-semibold mt-20  text-yellow-500">
+        CREATE PRODUCT
+      </h1>
+      <form className="mt-8 w-96" action="" style={{margin:'0 auto'}}> 
+      <div className="text-lg font-medium text-slate-50">
        <label> product Name: </label>
-       <input type="text" name="name" value={input.name} onChange={handleOnChange} />
-
+       <input   className="w-96 bg-grey-lighter text-2xl text-slate-950 py-2 rounded text-grey-darkest border border-grey-lighter rounded-l-none font-bold"
+       type="text" name="name" value={input.name} onChange={handleOnChange} />
+       </div>
+       <div className="text-lg font-medium text-slate-50">
        <label> product description </label>
-       <input type="text" name="description" value={input.description} onChange={handleOnChange} />
-
+       <input   className="w-96 bg-grey-lighter text-2xl text-slate-950 py-2 rounded text-grey-darkest border border-grey-lighter rounded-l-none font-bold"
+       type="text" name="description" value={input.description} onChange={handleOnChange} />
+       </div>
+       <div className="text-lg font-medium text-slate-50">
        <label> product price </label>
-       <input type="number" name="price" value={input.price} onChange={handleOnChange}/>
-
+       <input   className="w-96 bg-grey-lighter text-2xl text-slate-950 py-2 rounded text-grey-darkest border border-grey-lighter rounded-l-none font-bold"
+       type="number" name="price" value={input.price} onChange={handleOnChange}/>
+       </div>
+       <div className="text-lg font-medium text-slate-50">
        <label> Image </label>
-       <input type="file" onChange={handleImage}/>
-
-
-       <button onClick={handleSubmit} type='button'> submit </button>
-      </form>
+       <input  className="w-96 bg-grey-lighter text-2xl text-slate-950 py-2 rounded text-grey-darkest border border-grey-lighter rounded-l-none font-bold"
+       type="file" onChange={handleImage}/>
+       </div>
 
       <img src={image} weight="200" height={200} />
 
-    </div>
+       <button className="w-1/3 flex justify-center items-center active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-green-500 text-white text-lg font-bold" onClick={handleSubmit} type='button'> submit </button>
     
-  )
-}
-
+  
+      </form>
+      
+    </div>
+    </div>
+    </div>
+  )}
 export default FormProducts;
