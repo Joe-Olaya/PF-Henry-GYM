46b661ab0ad42ce164755af2ba7e364a572a0450
kevindel92 <kevindel_15@hotmail.com>
2023-05-11 01:46:25 -0300

    searchbar store

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -7,7 +7,8 @@ import {
   POST_REGISTER,
   ORDER_BY_NAME,
   POST_PRODUCT_CREATE,
-  ORDER_PRODUCTS
+  ORDER_PRODUCTS,
+  GET_NAME_PRODUCTS
 } from "./action_types";
 import axios from "axios";
 
@@ -112,6 +113,20 @@ export const filterByMuscle = (muscle) => {
 
 export function orderProducts(payload) {
   return { type: ORDER_PRODUCTS, payload };
-}
+};
+
+export function getNameProducts(name){
+  return async function (dispatch) {
+     try {
+       var json = await axios.get(`${URL}/products?name=${name}`);
+       console.log(json.data)
+       return dispatch ({
+          type: GET_NAME_PRODUCTS, 
+          payload: json.data
+       })
+      } catch (error) {
+        console.log(error.message)
+     }};
+};
 
 
