b84725d224691a8703e357613b59d5a50cbfaf42
Joe-Olaya <80336993+Joe-Olaya@users.noreply.github.com>
2023-05-24 02:08:15 -0300

    update exercises

--- api/src/db.js
+++ api/src/db.js
@@ -40,11 +40,11 @@ const { Exercise, Bodypart, Muscle, Routine, Product, User, Bodysale, Headersale
 // Routine.belongsToMany(Exercise, {through: 'Exercise_Routine'});
 // Exercise.belongsToMany(Routine, {through: 'Exercise_Routine'});
 
-// Muscle.hasMany(Exercise);
-// Exercise.belongsTo(Muscle);
-
 // Bodypart.hasMany(Exercise);
 // Exercise.belongsTo(Bodypart);
+
+Muscle.hasMany(Exercise);
+Exercise.belongsTo(Muscle);
 
 Headersale.hasMany(Bodysale);
 Bodysale.belongsTo(Headersale);
