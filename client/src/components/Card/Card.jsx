668b7c855781da38890807e3a4564790593a017e
Joe-Olaya <80336993+Joe-Olaya@users.noreply.github.com>
2023-05-23 23:57:14 -0300

    Update Card.jsx

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -6,16 +6,16 @@ const Card = (props) => {
         <img className="exerciseImg" src={props.gif_url}></img>
       <div className="info">
         <h1 className="title">
-          {props.name.charAt(0).toUpperCase() + props.name.slice(1)}
+          {props.name}
         </h1>
         {/* <p>Id: {props.id}</p> */}
         <div className="description">
         <h1>
-          Muscle: {props.muscle.charAt(0).toUpperCase() + props.muscle.slice(1)}
+          Muscle: {props.muscle}
         </h1>
         <h1>
           Body:{" "}
-          {props.body_part.charAt(0).toUpperCase() + props.body_part.slice(1)}
+          {props.body_part}
         </h1>
         </div>
       </div>
