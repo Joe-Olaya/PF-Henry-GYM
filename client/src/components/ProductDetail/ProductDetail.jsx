702604c45342d8b4b32c806611b5cf0ce645c83b
Abrildaine <abrudaine@gmail.com>
2023-05-25 16:06:29 -0300

    ProductDetail

--- client/src/components/ProductDetail/ProductDetail.jsx
+++ client/src/components/ProductDetail/ProductDetail.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 import StarRatings from "react-star-ratings";
 import axios from "axios";
 import "./ProductDetail.css";
 
 const ProductDetails = () => {
   const { productId } = useParams();
-  const rating = 4;
+  const [rating, setRating] = useState(0);
   const [quantity, setQuantity] = useState(1);
   const [actualCart, setActualCart] = useState(
     JSON.parse(localStorage.getItem("carrito")) || []
@@ -20,14 +21,41 @@ const ProductDetails = () => {
     state.products.find(product => product.id === parseInt(productId))
   );
 
+  useEffect(
+    () => {
+      const fetchComments = async () => {
+        try {
+          const response = await axios.get(`/reviews?productId=${productId}`);
+          setComments(response.data.comments);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      const fetchPunctuation = async () => {
+        try {
+          const response = await axios.get(
+            `/punctuation?productId=${productId}`
+          );
+          setRating(response.data.punctuation);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      fetchComments();
+      fetchPunctuation();
+    },
+    [productId]
+  );
+
   if (!product) {
     return <div>Loading...</div>;
   }
 
-  
-  const saveLocal = (item) => {
+  const saveLocal = item => {
     const updatedCart = [...actualCart, item];
-    localStorage.setItem('carrito', JSON.stringify(updatedCart));
+    localStorage.setItem("carrito", JSON.stringify(updatedCart));
     setActualCart(updatedCart);
   };
 
@@ -38,26 +66,40 @@ const ProductDetails = () => {
       const saveLocal = () => {
         localStorage.setItem("userId", JSON.stringify(userId));
       };
-      
-      
-      if (!userId) {
+
+      if (userId) {
+        console.log(userId);
+      } else if (!userId) {
         userId = null;
       }
 
       try {
         const response = await axios.post("/reviews", {
           review: newComment,
-          userId: userId
+          userId: userId,
+          punctuation: rating
         });
         console.log(response.data);
-        setComments(prevComments => [...prevComments, newComment]);
+        setComments((prevComments) => {
+          if (Array.isArray(prevComments)) {
+            return [...prevComments, newComment];
+          } else {
+            return [newComment];
+          }
+        });
         setCommentText("");
+        setRating(0);
       } catch (error) {
         console.log(error);
       }
     }
   };
-  
+
+  // const userId = JSON.parse(localStorage.getItem("userId")) || [];
+  // const saveLocal = () => {
+  //   localStorage.setItem("userId", JSON.stringify(userId));
+  // };
+
   return (
     <div className="divDetail">
       <div className="productDetail">
@@ -99,38 +141,42 @@ const ProductDetails = () => {
               </span>
               <button onClick={() => setQuantity(quantity + 1)}>+</button>
             </div>
-            <button className="cart" onClick={() => saveLocal({product,quantity})} >Add {quantity} to cart</button>
+            <button
+              className="cart"
+              onClick={() => saveLocal({ product, quantity })}
+            >
+              Add {quantity} to cart
+            </button>
           </div>
         </div>
       </div>
 
       <div className="reviews_producDetail">
         <div className="comments">
-          {comments.length === 0
-            ? <p className="commetsCount">No comments yet</p>
-            : comments.map((comment, index) =>
+          {comments && comments.length !== 0
+            ? comments.map((comment, index) =>
                 <div key={index} className="comments">
                   <p>
                     {comment}
                   </p>
                 </div>
-              )}
-          <div className="raiting" id="rating">
-            <input type="radio" id="star5" name="rating" value="5" />
-            <label for="star5" />
-            <input type="radio" id="star4" name="rating" value="4" />
-            <label for="star4" />
-            <input type="radio" id="star3" name="rating" value="3" />
-            <label for="star3" />
-            <input type="radio" id="star2" name="rating" value="2" />
-            <label for="star2" />
-            <input type="radio" id="star1" name="rating" value="1" />
-            <label for="star1" />
+              )
+            : <p className="commetsCount">No comments yet</p>}
+          <div className="stars-container">
+            <StarRatings
+              rating={rating}
+              starRatedColor="gold"
+              numberOfStars={5}
+              starDimension="20px"
+              starSpacing="2px"
+              changeRating={setRating}
+            />
           </div>
           <textarea
             placeholder="Add a comment"
             onChange={e => setCommentText(e.target.value)}
           />
+
           <button className="button_comments" onClick={addComment}>
             Add Comment
           </button>
